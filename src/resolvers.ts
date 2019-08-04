9f8b1d5ab8ea6f8e972481ad25e0965d2f267bdb
leonfers <leonfers@hotmail.com>
2019-08-03 23:08:16 -0300

    base

---
+++ src/resolvers.ts
@@ -0,0 +1,21 @@
+import {ResolverMap} from "./types/graphql-utils";
+import {GQL} from "./types/schema";
+import * as bcrypt from 'bcryptjs';
+import {User} from "./entity/User";
+
+export const resolvers: ResolverMap = {
+    Query: {
+        hello: (_, {name}: GQL.IHelloOnQueryArguments) => `bye ${name || 'World'}`,
+    },
+    Mutation: {
+        register: async (_, {email, password}: GQL.IRegisterOnMutationArguments) => {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const user = User.create({
+                email,
+                password: hashedPassword
+            });
+            await user.save();
+            return true;
+        }
+    }
+};
---
+++ src/types/schema.ts
@@ -0,0 +1,44 @@
+// tslint:disable
+// graphql typescript definitions
+
+export declare namespace GQL {
+    export interface IGraphQLResponseRoot {
+        data?: IQuery | IMutation;
+        errors?: Array<IGraphQLResponseError>;
+    }
+
+    export interface IGraphQLResponseError {
+        /** Required for all errors */
+        message: string;
+        locations?: Array<IGraphQLResponseErrorLocation>;
+
+        /** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
+        [propName: string]: any;
+    }
+
+    export interface IGraphQLResponseErrorLocation {
+        line: number;
+        column: number;
+    }
+
+    export interface IQuery {
+        __typename: "Query";
+        hello: string;
+    }
+
+    export interface IHelloOnQueryArguments {
+        name?: string | null;
+    }
+
+    export interface IMutation {
+        __typename: "Mutation";
+        register: boolean | null;
+    }
+
+    export interface IRegisterOnMutationArguments {
+        email: string;
+        password: string;
+    }
+}
+
+// tslint:enable
